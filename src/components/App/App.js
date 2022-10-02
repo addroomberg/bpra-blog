7cab4107bd26f00a893aab50ba050f09eb675634
Andrew Rymar <rymarandrew@gmail.com>
2022-10-02 14:34:43 +0300

    Added basic components and containers

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Routes from '../../routes/routes';
+import styles from './App.module.css'
+
+const App = () => (
+  <div className={styles.app}>
+    <Routes />
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import App from './components/App/App';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+import { watchAuth, watchUsers } from "./store/sagas";
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchUsers);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
